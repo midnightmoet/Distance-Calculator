b72c341451c906d295a32c3b323c3e9adcc5fcee
Lisa Miner <midnightmoet@gmail.com>
2023-06-15 11:37:55 -0400

    Functioning app, want to add a map later

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### Distance Calculator
 
-
 ## Description
 
 Calculate distance from point A to point B using latitude and longitude.
 
+# Note: The distance between Point A and Point B is calculated using the Haversine formula,
+which is used to calculate the shortest distance between two points on a perfect sphere.
+Given that the Earth is not a perfect sphere, the distance calculated here is approximate.
+[Educative.io](https://www.educative.io/answers/how-to-calculate-distance-using-the-haversine-formula)
--- script.js
+++ script.js
@@ -1,106 +1,82 @@
-function calculateDistance(e) {
-    e.preventDefault();
-
-    let location1Input = document.getElementById('location1').value;
-    let location2Input = document.getElementById('location2').value;
-
-    // Call an API or use a library to convert the location inputs into latitude and longitude coordinates.
-    // For example, you can use the Google Geocoding API or a geocoding library like OpenCageData or Nominatim.
-    // Refer to the documentation of your chosen provider for instructions on how to do this.
-
-    let encodedAddress1 = encodeURIComponent(location1Input);
-    let encodedAddress2 = encodeURIComponent(location2Input);
-
-    let apiKey = "AIzaSyAED2cZlyb4qUJRzLW7Du7OG71uQsCTlB0"; //GOOGLE MAPS API; created my own and need to modify so it will work????
-
-// API URL
-    let apiUrl1 = "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodedAddress1 + "&key=" + apiKey;
-    let apiUrl2 = "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodedAddress2 + "&key=" + apiKey;
-
-    let latitude1;
-    let longitude1;
-    let latitude2;
-    let longitude2;
-
-// FETCH API
-    let promise1 = fetch(apiUrl1)
-        .then(function(response){
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error("Network response was not ok.");
-        })
-        .then(function(data){
-            if (data.status === "OK" && data.results.length > 0) {
-                let location1 = data.results[0].geometry.location;
-                latitude1 = location1.lat;
-                longitude1 = location1.lng;
-
-                console.log("Latitude1:", latitude1);
-                console.log("Longitude1:", longitude1);
-            } else {
-                console.log("No results found.");
-            }
-        })
-        .catch(function(error){
-            console.log("Error:", error.message);
-        });
-
-    let promise2 = fetch(apiUrl2)
-        .then(function(response) {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error("Network response was not ok.");
-        })
-        .then(function(data) {
-            if (data.status === "OK" && data.results.length > 0) {
-                let location2 = data.results[0].geometry.location;
-                latitude2 = location2.lat;
-                longitude2 = location2.lng;
-
-                console.log("Latitude2:", latitude2);
-                console.log("Longitude2:", longitude2);
-            } else {
-                console.log("No results found.");
-            }
-        })
-        .catch(function(error){
-            console.log("Error:", error.message);
-        });
-
-    Promise.all([promise1, promise2])
-        .then(function(){
-            let distance = calculateDistance(latitude1, longitude1, latitude2, longitude2);
-            displayResult(distance);
-        });
-}
-
-function calculateDistance(lat1, lon1, lat2, lon2) {
-    // Use the Haversine formula to calculate the distance between two points.
-
-    let R = 3958; // Radius of the Earth in miles
-    let dLat = degToRad(lat2 - lat1);
-    let dLon = degToRad(lon2 - lon1);
-
+function degree2Radius(degree) {
+    return degree * (Math.PI / 180);
+  }
+  
+  function getDistanceFromLatLonInKm() {
+    let latitude1, longitude1, latitude2, longitude2;
+  
+    const getValues = () => {
+      let aCoordinates = document.getElementById("a").value;
+      let bCoordinates = document.getElementById("b").value;
+  
+      let aCoordArray = aCoordinates.replace(/\s/g, "").split(",");
+      let bCoordArray = bCoordinates.replace(/\s/g, "").split(",");
+  
+      latitude1 = aCoordArray[0];
+      longitude1 = aCoordArray[1];
+      latitude2 = bCoordArray[0];
+      longitude2 = bCoordArray[1];
+    };
+  
+    getValues();
+  
+    console.log(latitude1, longitude1, latitude2, longitude2);
+  
+    let R = 3963; // Earth's radius in miles
+    let dLat = degree2Radius(latitude2 - latitude1); // deg2rad below
+    let dLon = degree2Radius(longitude2 - longitude1);
     let a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(degree2Radius(latitude1)) *
+        Math.cos(degree2Radius(latitude2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    let distance = R * c; // Distance in miles
-    return distance;
-}
-
-function degToRad(deg) {
-    return deg * (Math.PI / 180);
-}
-
-function displayResult(distance) {
-    let resultElement = document.getElementById('result');
-    resultElement.innerHTML = 'Distance: ' + distance.toFixed(2) + ' miles';
-}
-
-let distanceForm = document.getElementById('distanceForm');
-distanceForm.addEventListener('submit', calculateDistance);
+    let d = (R * c).toFixed(2); // Distance in miles
+  
+    if (d === "NaN"){
+      alert('Coordinates are not valid. Please try again.')
+      document.getElementById("result").innerHTML = '';
+      return;
+    }
+  
+    document.getElementById("result").innerHTML = `Distance: ${d} miles`;
+  }
+  
+  
+  
+  document.addEventListener("DOMContentLoaded", (e) => {
+    document.getElementById("fTwo").addEventListener("submit",  (e) => {
+      e.preventDefault(); 
+      getDistanceFromLatLonInKm();
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded",( e )=> {
+    document.getElementById("fOne").addEventListener("submit", function (e) {
+      e.preventDefault(); 
+      let address = document.getElementById("address").value;
+      console.log(address);
+      getAddress(address);
+    });
+  });
+  
+  
+  
+  const getAddress = async (address) => {
+  
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode( {'address': address}, function(results, status) {
+      if (status == 'OK') {
+        var latitude = results[0].geometry.location.lat();
+        var longitude = results[0].geometry.location.lng();
+        console.log(latitude, longitude)
+        document.getElementById("latlon").innerHTML=`(latitude, longitude) = (${latitude}, ${longitude})`;
+  
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+        document.getElementById("latlon").innerHTML='';
+      }
+    })};
+    
+  function initMap() {};
